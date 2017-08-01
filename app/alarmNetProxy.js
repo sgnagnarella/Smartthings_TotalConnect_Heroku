9d46685c0323e338acd2c700e564bcad9bd39512
Sebastian Gnagnarella <sgnagnarella@Sebastians-MacBook-Pro.local>
2017-08-01 17:57:33 -0600

    Methods added to proxy

--- app/alarmNetProxy.js
+++ app/alarmNetProxy.js
@@ -16,8 +16,26 @@ function logout(req, process){
     service.post('/TC21API/TC2.asmx/Logout', req, process)
 }
 
+function getPanelMetaDataAndFullStatus(req, process){
+    console.log('proxy.getPanelMetaDataAndFullStatus')
+    service.post('/TC21API/TC2.asmx/GetPanelMetaDataAndFullStatus', req, process)
+}
+
+function armSecuritySystem(req, process){
+    console.log('proxy.armSecuritySystem')
+    service.post('/TC21API/TC2.asmx/ArmSecuritySystem', req, process)
+}
+
+function disarmSecuritySystem(req, process){
+    console.log('proxy.disarmSecuritySystem')
+    service.post('/TC21API/TC2.asmx/DisarmSecuritySystem', req, process)
+}
 
 module.exports.login = login
 module.exports.getSessionDetails = getSessionDetails
 module.exports.logout = logout
+module.exports.getPanelMetaDataAndFullStatus = getPanelMetaDataAndFullStatus
+module.exports.armSecuritySystem = armSecuritySystem
+module.exports.disarmSecuritySystem = disarmSecuritySystem
 
+ 
